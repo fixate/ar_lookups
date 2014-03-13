b64269cc7932bda5a3bbe1543c5f4f221e8fe336
Stan Bondi <stan@fixate.it>
2014-03-13 16:25:15 +0200

    Added eager_load! method to keep production rails happy

--- lib/ar_lookups.rb
+++ lib/ar_lookups.rb
@@ -4,3 +4,8 @@ require 'ar_lookups/version'
 require 'ar_lookups/lookup_table_definition'
 require 'ar_lookups/base_ext'
 require 'ar_lookups/railtie' if defined?(Rails)
+
+module ArLookups
+  def self.eager_load!
+  end
+end
